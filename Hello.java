cd0d27b85d0973ffd18425ee96f4217ccf8221fe
Avinash1311 <46309977+Avinash1311@users.noreply.github.com>
2019-02-12 11:27:47 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Hello{
+public static void main(String[] qwe){
+
+Scanner scan=new Scanner(System.in);
+
+int n=scan.nextInt(),x=1;
+
+while(x<=n){
+System.out.println("Hello");
+x++;
+}
+
+}
+
+}
