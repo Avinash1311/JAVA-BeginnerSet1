45b5101175b2208623d5887f82f01996022f9487
Avinash1311 <46309977+Avinash1311@users.noreply.github.com>
2019-02-12 11:33:19 +0530

    Create LeapYear.java

---
+++ LeapYear.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class LeapYear{
+public static void main(String[] qwe){
+
+Scanner scan=new Scanner(System.in);
+
+int year=scan.nextInt();
+
+if(year%4==0){
+
+if(year%100==0){
+
+if(year%400==0)
+System.out.println("Yes");
+
+else 
+System.out.println("no");
+}
+
+else
+System.out.println("Yes");
+}
+
+else
+System.out.println("no");
+ 
+}
+
+}
