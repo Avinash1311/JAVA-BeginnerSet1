89705d0787f4bbef6adce169581c25b7eccc643c
Avinash1311 <46309977+Avinash1311@users.noreply.github.com>
2019-02-12 11:25:48 +0530

    Create Alpha.java

---
+++ Alpha.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Alpha{
+public static void main(String[] qwe){
+
+Scanner scan=new Scanner(System.in);
+
+char n=scan.next().charAt(0);
+
+if(Character.isLetter(n))
+System.out.println("Alphabet");
+
+else
+System.out.println("No");
+
+}
+
+}
