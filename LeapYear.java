261fb2e28235789baf10b8561905c0b2988f3b21
Avinash1311 <46309977+Avinash1311@users.noreply.github.com>
2019-02-12 19:26:28 +0530

    Update LeapYear.java

--- LeapYear.java
+++ LeapYear.java
@@ -11,14 +11,14 @@ if(year%4==0){
 if(year%100==0){
 
 if(year%400==0)
-System.out.println("Yes");
+System.out.println("yes");
 
 else 
 System.out.println("no");
 }
 
 else
-System.out.println("Yes");
+System.out.println("yes");
 }
 
 else
